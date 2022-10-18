9d8a88c8a2a3b8a998ab864579a5d8e303c56375
Delane Codes <58907512+delane-codes@users.noreply.github.com>
2022-10-18 04:26:32 +0200

    UAV class

---
+++ UAV/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module suav.uav {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens suav.uav to javafx.fxml;
+    exports suav.uav;
+}
